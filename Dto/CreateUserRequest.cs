c9d8de6a1417941e4e204a8926307fa92d77c911
Ahmed Hussien <ahmedhgabr25@gmail.com>
2026-01-06 01:46:38 +0200

    feat: implement dtos

---
+++ Dto/CreateUserRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace RegistrationForm.Dto
+{
+    public class CreateUserRequest
+    {
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public string? Phone { get; set; } = null;
+        public required int Age { get; set; }
+    }
+}
---
+++ Dto/UpdateUserRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace RegistrationForm.Dto
+{
+    public class UpdateUserRequest
+    {
+        public required string Email { get; set; }
+        public string? UpdatedName { get; set; }
+        public string? UpdatedEmail { get; set; }
+        public string? UpdatedPhone { get; set; } = null;
+        public int? UpdatedAge { get; set; }
+    }
+}
