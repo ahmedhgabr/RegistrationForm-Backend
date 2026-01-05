0577e015746fbb21086004f125c112cc84916895
Ahmed Hussien <ahmedhgabr25@gmail.com>
2026-01-05 18:33:49 +0200

    feat: add scalar

--- Program.cs
+++ Program.cs
@@ -1,12 +1,23 @@
+using Microsoft.Extensions.DependencyInjection;
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 
+// Add OpenAPI services
+builder.Services.AddOpenApi();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference();
+}
 
 app.UseHttpsRedirection();
 
